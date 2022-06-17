2bc21e3542e49f92670d4b1b161594861e61f32b
Miriam Anyango <minaoanya@gmail.com>
2022-06-17 16:47:49 +0300

    Ensure the Task LIster is running as it should

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', function (event){
+    event.preventDefault()
+    let ul = document.querySelector('#tasks')
+    let li = document.createElement('li')
+    let input = document.querySelector('#new-task-description')
+    li.textContent = input.value
+    ul.appendChild(li)
+    form.reset()
+  })
 });
+
